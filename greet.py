415f473d2844d8c1cfc29a597e05b6e4469679d0
Hedy Li <50042066+hedyhyry@users.noreply.github.com>
2020-04-06 16:39:25 +0800

    add parameter and stuff to morning command in greet.py

--- greet.py
+++ greet.py
@@ -11,8 +11,10 @@ def greet(punc, name):
     print("Hello, ", name.capitalize(), punc, sep="")
 
 @cli.command()
-def morning():
-    click.echo("Good morning, ")
+@click.option('--punc', default="!", help="Set the punctuation at the end")
+@click.argument('name', required=False, default="User")
+def morning(name, punc):
+    print("Good morning, ", name.capitalize(), punc, sep="")
 
 if __name__ == '__main__':
     cli()
