e94425e3fe3354292eb34e9f61aea9955a03c217
Hedy Li <50042066+hedyhyry@users.noreply.github.com>
2020-04-07 15:34:18 +0800

    add new options to greet
    -u, --upper
    -l, --lower
    -w, --word
    all help messages done too.

--- greet.py
+++ greet.py
@@ -5,10 +5,22 @@ def cli():
     pass
 
 @cli.command()
-@click.option('--punc', default="!", help="Set the punctuation at the end")
+@click.option('-p', '--punc', default="!", help="Set the punctuation at the end")
+@click.option('-u', '--upper', default=False, \
+                is_flag=True, help="Change the message to all uppercase")
+@click.option('-l', '--lower', default=False, \
+                is_flag=True, help="Change the message to all lowercase")
+@click.option('-w', '--word', required=False, default="Hello", \
+                help="Set the greeting word, e.g.: Hi")
 @click.argument('name', required=False, default="World")
-def greet(punc, name):
-    print("Hello, ", name.capitalize(), punc, sep="")
+def greet(punc, name, upper, lower, word):
+    string = f"{word.capitalize()}, {name.capitalize()}{punc}"
+    if upper:
+        print(string.upper())
+    elif lower:
+        print(string.lower())
+    else:
+        print(string)
 
 @cli.command()
 @click.option('--punc', default="!", help="Set the punctuation at the end")
